3b5c194368ed069c0d8761866a2afc2959b309d7
yu-tcy <77433484+yu-tcy@users.noreply.github.com>
2021-05-04 16:05:57 +0900

    商品情報編集機能_1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :check_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,20 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if @item.user == current_user
-      render :edit
-    else
-      redirect_to root_path
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -50,9 +43,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :text, :category_id, :status_id, :postage_id, :area_id, :day_id, :image, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_user
+    if @item.user == current_user
+      render :edit
+    else
+      redirect_to root_path
     end
   end
+
 end
