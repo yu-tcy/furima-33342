4085e742405148a27f40779997354afb592df2b9
yu-tcy <77433484+yu-tcy@users.noreply.github.com>
2021-05-18 21:22:13 +0900

    商品購入機能_4

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,9 +2,6 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal,:city,:address,:apart,:number, :area_id, :user_id, :item_id, :token
 
-  # ここにバリデーションの処理を書く
-
-
   with_options presence: true do
     validates :token
     validates :postal,      format: {with:/\A\d{3}[-]\d{4}\z/,  message: "is invalid. Please input full-width characters."}
@@ -20,7 +17,6 @@ class OrderAddress
   end
 
   def save
-    # 各テーブルにデータを保存する処理を書く
     @order_address = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal: postal, area_id: area_id, city: city, area_id: area_id, address: address, apart: apart, number: number, order_id: @order_address.id)
   end
