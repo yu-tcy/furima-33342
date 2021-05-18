23111e1ff0651bf9b1687977b2d91f907a6d5b24
yu-tcy <77433484+yu-tcy@users.noreply.github.com>
2021-05-18 21:01:07 +0900

    商品購入機能_3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :check, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+    before_action :check, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -28,7 +28,6 @@ class OrdersController < ApplicationController
     end
 
     def check
-      @item = Item.find(params[:item_id])
       if @item.user_id == current_user.id || @item.order.present?
         redirect_to root_path
       end
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     number      {"09012345678"}
     area_id     {2}
     token {"tok_abcdefghijk00000000000000000"}
-    user_id     {1}
-    item_id     {1}
   end
 end
